b16a730e8510bb520330e8aeb069e941e386989f
OmarHernandezRey <hernandezreyomar@gmail.com>
2023-12-06 18:19:50 -0500

    05 Anatomía de una variable

---
+++ clase-5-variables.js
@@ -0,0 +1,12 @@
+let cajaDeAndy='Woody'
+console.log(cajaDeAndy)
+
+//No permitido
+let c='Woody'
+let cda='Woody'
+let pcAndy='Woody'
+
+//Permitido
+let primerTrasteoDeAndy='Woody'
+let urlDelUsuario='https://www.google.com'
+let idDelUsuario='123456789';
