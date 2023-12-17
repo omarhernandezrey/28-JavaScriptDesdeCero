b747a4d3bf47d03796b621cab46f61fd2dab9b12
OmarHernandezRey <hernandezreyomar@gmail.com>
2023-12-17 12:18:34 -0500

    22 Identificador this

--- clase-22-identificador-this.js
+++ clase-22-identificador-this.js
@@ -1,29 +1,39 @@
-// Enlace implicito -> Implicit binding
+// Enlace implícito · Implicit binding
 
+// Objeto 'house' con una propiedad 'dogName' y un método 'dogGreeting'.
 const house = {
     dogName: 'Fido',
-    dogGreeting: function  () {
-        console.log(`Hi, I'm ${this.dogName}`);
+    dogGreeting: function () {
+      console.log(`Hi, I'm ${this.dogName}`);
     }
 }
-house.dogGreeting()
 
-// Enlace Explicito -> Explicit binding
-function  dogGreeting() {
+// Llamada al método 'dogGreeting' usando el enlace implícito.
+house.dogGreeting();
+
+// Enlace explícito · Explicit binding
+
+// Función 'dogGreeting' que se utilizará con el enlace explícito.
+function dogGreeting() {
     console.log(`Hi, I'm ${this.dogName}`);
 }
 
+// Nuevo objeto 'newHouse' con una propiedad 'dogName'.
 const newHouse = {
-    dogName: 'Lucas',
-    dogAge: 3
+    dogName: 'Coconut',
 }
 
-dogGreeting.call(newHouse)
+// Llamada a la función 'dogGreeting' usando el método 'call' para establecer el contexto al objeto 'newHouse'.
+dogGreeting.call(newHouse);
 
-function  newdogGreeting(owner, address) {
-    console.log(`Hi, I'm ${this.dogName} and I live whith ${owner} on ${address}`);
+// Función 'newDogGreeting' que toma dos parámetros ('owner' y 'address') e imprime un mensaje con el nombre del perro y la información proporcionada.
+function newDogGreeting(owner, address) {
+    console.log(`Hi, I'm ${this.dogName} and I live with ${owner} on ${address}`);
 }
 
-const owner = 'Lucy'
-const address = 'carrera2N#39d-16sur'
-newdogGreeting().call(newHouse. owner, address)
+// Definición de valores para 'owner' y 'address'.
+const owner = 'Lucy';
+const address = 'Avenue 123';
+
+// Llamada a la función 'newDogGreeting' usando el método 'call' para establecer el contexto al objeto 'newHouse' y pasar los valores de 'owner' y 'address' como argumentos adicionales.
+newDogGreeting.call(newHouse, owner, address);
