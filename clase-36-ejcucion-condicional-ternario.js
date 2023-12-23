d844d379adda10fe499c893414bfb0ea49421040
OmarHernandezRey <hernandezreyomar@gmail.com>
2023-12-23 17:10:31 -0500

    36 Ejecución condicional: ternario

--- clase-36-ejcucion-condicional-ternario.js
+++ clase-36-ejcucion-condicional-ternario.js
@@ -1,18 +1,13 @@
 // Evaluación de la edad para permitir el acceso
-
 let mensaje;  // Variable para almacenar el mensaje de acceso
-
 const edad = 20;
-
 // Uso de un bloque if-else para determinar el mensaje según la edad
 if (edad >= 18) {
     mensaje = "Es mayor de edad, puede pasar";
 } else {
     mensaje = "No es mayor de edad, no puede pasar";
 }
-
 console.log(mensaje);
-
 // Uso de la operación ternaria para lograr lo mismo de manera más concisa
 const edad1 = 20;
 const mensaje1 = edad1 >= 18 ? "Es mayor de edad, puede pasar" : "No es mayor de edad, no puede pasar";
