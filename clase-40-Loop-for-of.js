28f66de335193f1f138038f617a212f780bc7ef9
OmarHernandezRey <hernandezreyomar@gmail.com>
2023-12-26 15:58:15 -0500

    40 Loop: for of

--- clase-40-Loop-for-of.js
+++ clase-40-Loop-for-of.js
@@ -1,14 +1,17 @@
 /*
-for of arrays, strings [slgo]
+El bucle for...of se utiliza para iterar sobre elementos de iterable, como arrays o cadenas.
 
 for (variable of objeto) {
-     codigo a ajecutar
+    // código a ejecutar para cada elemento del objeto
 }
 */
 
-let canasta = ["manzana", "pera", "manzana", "uva"]
+// Se declara una variable llamada canasta que contiene un array de strings.
+let canasta = ["manzana", "pera", "manzana", "uva"];
 
+// Se utiliza el bucle for...of para iterar sobre cada elemento del array canasta.
 for (fruta of canasta) {
+    // En cada iteración, se ejecuta el código proporcionado, que imprime el array completo en lugar del elemento actual.
     console.log(canasta);
 }
 
