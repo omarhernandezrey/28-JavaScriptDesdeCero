ca98e36ef65e9f0b38b9a57be0560e0d4b300d62
OmarHernandezRey <hernandezreyomar@gmail.com>
2023-12-26 15:52:51 -0500

    40 Loop: for of

---
+++ clase-40-Loop-for-of.js
@@ -0,0 +1,15 @@
+/*
+for of arrays, strings [slgo]
+
+for (variable of objeto) {
+     codigo a ajecutar
+}
+*/
+
+let canasta = ["manzana", "pera", "manzana", "uva"]
+
+for (fruta of canasta) {
+    console.log(canasta);
+}
+
+
